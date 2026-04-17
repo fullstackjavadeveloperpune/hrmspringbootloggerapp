afa2132d0856cdf7dbe555b2c9aac4bc5bc0218c
rushabhr <rushabhr2131@gmail.com>
2026-04-17 10:32:36 +0530

    Findbyid endpoint added

--- hrmex/src/main/java/com/fullstack/service/EmployeeService.java
+++ hrmex/src/main/java/com/fullstack/service/EmployeeService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,5 +30,10 @@ public class EmployeeService implements IEmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeRepo.findAll();
+    }
+
+    @Override
+    public Optional<Employee> findById(long empId) {
+        return employeeRepo.findById(empId);
     }
 }
